a5fbaa7248f92ea2bcb4aeb71e95db118e279cf2
MUTCHUMARI CHANDRASEKHAR REDDY <248098965+chandumutchumari@users.noreply.github.com>
2026-08-15 23:24:06 +0530

    Add seriessum2.c to calculate power series sum

---
+++ seriessum2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+    int x,n,i;
+    double sum=0;
+
+    printf("Enter the values of x and n: ");
+    scanf("%d %d",&x,&n);
+
+    for(i=0;i<=n;i++){
+        sum+=pow(x,i);
+    }
+
+    printf("The sum of the series is: %lf",sum);
+    return 0;
+}
